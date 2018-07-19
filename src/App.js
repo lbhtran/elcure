6c91c5ba7e8cc353f618911a6daa1dd7aeee3bb9
Alexx <lbhtran@gmail.com>
2018-07-19 23:53:07 +0100

    Timeline

--- src/App.js
+++ src/App.js
@@ -6,68 +6,53 @@ class App extends Component {
       <div className="App">
       <div id="timeline">
         <ul id="dates">
-          <li><a href="#11th July">11th July</a></li>
-          <li><a href="#12th July">12th July</a></li>
-          <li><a href="#13th July">13th July</a></li>
-          <li><a href="#14th July">14th July</a></li>
-          <li><a href="#15th July">15th July</a></li>
-          <li><a href="#16th July">16th July</a></li>
-          <li><a href="#17th July">17th July</a></li>
-          <li><a href="#18th July">18th July</a></li>
-          <li><a href="#19th July">19th July</a></li>
-          <li><a href="#20th July">20th July</a></li>
+          <li><a href="#day0">Day 0</a></li>
+          <li><a href="#day1">Day 1</a></li>
+          <li><a href="#day2">Day 2</a></li>
+          <li><a href="#day3">Day 3</a></li>
+          <li><a href="#day4">Day 4</a></li>
+          <li><a href="#day5">Day 5</a></li>
+          <li><a href="#day6">Day 6</a></li>
+          <li><a href="#day7">Day 7</a></li>
+          <li><a href="#day8">Day 8</a></li>
+          <li><a href="#day9">Day 9</a></li>
         </ul>
         <ul id="issues">
-          <li id="1900">
+          <li id="#day0">
             <img src="images/11.jpg" width="100" height="100" />
-            <p>Unfortunately, I was diagnosed with leukaemia today :(. I don't know anyone who is in the same boat as me. Would appreciate any help and advice. </p>
-            <img src="images/1.png" width="100" height="100" />
+            <p>Unfortunately, I was diagnosed with leukaemia today :(.</p>
+            <img src="images/1.PNG" width="100" height="100" />
             <p>  .</p>
           </li>
-          <li id="1930">
-            <img src="images/2.png" width="256" height="256" />
-            <h1>1930</h1>
-            <p>Donec semper quam scelerisque tortor dictum gravida. In hac habitasse platea dictumst. Nam pulvinar, odio sed rhoncus suscipit, sem diam ultrices mauris, eu consequat purus metus eu velit. Proin metus odio, aliquam eget molestie nec, gravida ut sapien. Phasellus quis est sed turpis sollicitudin venenatis sed eu odio. Praesent eget neque eu eros interdum malesuada non vel leo. Sed fringilla porta ligula.</p>
+          <li id="#day1">
+            <img src="images/2.png" width="100" height="100" />
+            <p>Donec semper quam scelerisque tortor dictum gravida.</p>
           </li>
-          <li id="1944">
-            <img src="images/3.png" width="256" height="256" />
-            <h1>1944</h1>
-            <p>Donec semper quam scelerisque tortor dictum gravida. In hac habitasse platea dictumst. Nam pulvinar, odio sed rhoncus suscipit, sem diam ultrices mauris, eu consequat purus metus eu velit. Proin metus odio, aliquam eget molestie nec, gravida ut sapien. Phasellus quis est sed turpis sollicitudin venenatis sed eu odio. Praesent eget neque eu eros interdum malesuada non vel leo. Sed fringilla porta ligula.</p>
+          <li id="#day2">
+            <img src="images/3.png" width="100" height="100" />
+            <p>Donec semper quam scelerisque tortor dictum gravida.</p>
           </li>
-          <li id="1950">
-            <img src="images/4.png" width="256" height="256" />
-            <h1>1950</h1>
-            <p>Donec semper quam scelerisque tortor dictum gravida. In hac habitasse platea dictumst. Nam pulvinar, odio sed rhoncus suscipit, sem diam ultrices mauris, eu consequat purus metus eu velit. Proin metus odio, aliquam eget molestie nec, gravida ut sapien. Phasellus quis est sed turpis sollicitudin venenatis sed eu odio. Praesent eget neque eu eros interdum malesuada non vel leo. Sed fringilla porta ligula.</p>
+          <li id="#day3">
+            <img src="images/4.png" width="100" height="100" />
+            <p>Donec semper quam scelerisque tortor dictum gravida.</p>
           </li>
-          <li id="1971">
-            <img src="images/5.png" width="256" height="256" />
-            <h1>1971</h1>
-            <p>Donec semper quam scelerisque tortor dictum gravida. In hac habitasse platea dictumst. Nam pulvinar, odio sed rhoncus suscipit, sem diam ultrices mauris, eu consequat purus metus eu velit. Proin metus odio, aliquam eget molestie nec, gravida ut sapien. Phasellus quis est sed turpis sollicitudin venenatis sed eu odio. Praesent eget neque eu eros interdum malesuada non vel leo. Sed fringilla porta ligula.</p>
+          <li id="#day4">
           </li>
-          <li id="1977">
-            <img src="images/6.png" width="256" height="256" />
-            <h1>1977</h1>
-            <p>Donec semper quam scelerisque tortor dictum gravida. In hac habitasse platea dictumst. Nam pulvinar, odio sed rhoncus suscipit, sem diam ultrices mauris, eu consequat purus metus eu velit. Proin metus odio, aliquam eget molestie nec, gravida ut sapien. Phasellus quis est sed turpis sollicitudin venenatis sed eu odio. Praesent eget neque eu eros interdum malesuada non vel leo. Sed fringilla porta ligula.</p>
+          <li id="#day5">
+            <img src="images/5.png" width="100" height="100" />
+            <p>Donec semper quam scelerisque tortor dictum gravida.</p>
           </li>
-          <li id="1989">
-            <img src="images/7.png" width="256" height="256" />
-            <h1>1989</h1>
-            <p>Donec semper quam scelerisque tortor dictum gravida. In hac habitasse platea dictumst. Nam pulvinar, odio sed rhoncus suscipit, sem diam ultrices mauris, eu consequat purus metus eu velit. Proin metus odio, aliquam eget molestie nec, gravida ut sapien. Phasellus quis est sed turpis sollicitudin venenatis sed eu odio. Praesent eget neque eu eros interdum malesuada non vel leo. Sed fringilla porta ligula.</p>
+          <li id="#day6">
+            <img src="images/6.png" width="100" height="100" />
+            <p>Donec semper quam scelerisque tortor dictum gravida.</p>
           </li>
-          <li id="1999">
-            <img src="images/8.png" width="256" height="256" />
-            <h1>1999</h1>
-            <p>Donec semper quam scelerisque tortor dictum gravida. In hac habitasse platea dictumst. Nam pulvinar, odio sed rhoncus suscipit, sem diam ultrices mauris, eu consequat purus metus eu velit. Proin metus odio, aliquam eget molestie nec, gravida ut sapien. Phasellus quis est sed turpis sollicitudin venenatis sed eu odio. Praesent eget neque eu eros interdum malesuada non vel leo. Sed fringilla porta ligula.</p>
+          <li id="#day7">
           </li>
-          <li id="2001">
-            <img src="images/9.png" width="256" height="256" />
-            <h1>2001</h1>
-            <p>Donec semper quam scelerisque tortor dictum gravida. In hac habitasse platea dictumst. Nam pulvinar, odio sed rhoncus suscipit, sem diam ultrices mauris, eu consequat purus metus eu velit. Proin metus odio, aliquam eget molestie nec, gravida ut sapien. Phasellus quis est sed turpis sollicitudin venenatis sed eu odio. Praesent eget neque eu eros interdum malesuada non vel leo. Sed fringilla porta ligula.</p>
+          <li id="#day8">
           </li>
-          <li id="2011">
-            <img src="images/10.png" width="256" height="256" />
-            <h1>2011</h1>
-            <p>Donec semper quam scelerisque tortor dictum gravida. In hac habitasse platea dictumst. Nam pulvinar, odio sed rhoncus suscipit, sem diam ultrices mauris, eu consequat purus metus eu velit. Proin metus odio, aliquam eget molestie nec, gravida ut sapien. Phasellus quis est sed turpis sollicitudin venenatis sed eu odio. Praesent eget neque eu eros interdum malesuada non vel leo. Sed fringilla porta ligula.</p>
+          <li id="#day9">
+            <img src="images/8.png" width="100" height="100" />
+            <p>Donec semper quam scelerisque tortor dictum gravida.</p>
           </li>
       </ul>
       <div id="grad_left"></div>
